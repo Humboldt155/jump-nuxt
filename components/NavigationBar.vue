6c5acc9e4ce8baec44a3a0c8d2fb5571b04c0931
Humboldt155 <petr_kozyrev@icloud.com>
2018-02-20 21:48:04 +0300

    Initial commit

---
+++ components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar is-transparent" >
+    <div class="navbar-brand" >
+      <a class="navbar-item" >
+        Jump
+      </a>
+      <a class="button is-primary is-small is-rounded">BETA</a>
+    </div>
+    <div class="navbar-end">
+      <b-dropdown position="is-bottom-left">
+        <button class="button is-primary is-small" slot="trigger">
+          <span>RU</span>
+        </button>
+        <b-dropdown-item>RU</b-dropdown-item>
+        <b-dropdown-item>EN</b-dropdown-item>
+        <b-dropdown-item>FR</b-dropdown-item>
+      </b-dropdown>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+        name: "navigation-bar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NavigationBar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+  import NavigationBar from '~/components/NavigationBar.vue'
+  export default {
+    components: {
+      NavigationBar
+    }
+  }
+</script>
